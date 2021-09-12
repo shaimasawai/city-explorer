0d75e5bb4f1b215a17ef65a98c5b750eb895ae33
shaimasawai <shaima.alsawaie@gmail.com>
2021-09-12 16:52:39 -0400

    solve the lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { Component } from "react";
+import Form from "./component/Form";
+import Location from "./component/Location";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      lon: "",
+      lat: "",
+      type: "",
+      showData: false,
+    };
+  }
+  handlerName = (e) => {
+    let name = e.target.value;
+    this.setState({
+      name: name,
+    });
+  };
+  submitHandler = (e) => {
+    e.preventDefault();
+    let confing = {
+      method: "GET",
+      baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.name}`,
+    };
+    axios(confing).then((res) => {
+      let firstLOcation = res.data[0];
+      this.setState({
+        name: firstLOcation.display_name,
+        lon: firstLOcation.lon,
+        lat: firstLOcation.lat,
+        type: firstLOcation.type,
+        showData: true,
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Form
+          handlerName={this.handlerName}
+          submitHandler={this.submitHandler}
+        />
+        {this.state.showData && (
+          <Location
+            name={this.state.name}
+            lon={this.state.lon}
+            lat={this.state.lat}
+            type={this.state.type}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
